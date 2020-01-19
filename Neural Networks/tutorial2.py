97f1a0731976498589e034464d699ab4b813de01
naveen <naveen9958599096@gmail.com>
2020-01-19 13:26:46 +0530

    added saved model

--- Neural Networks/tutorial2.py
+++ Neural Networks/tutorial2.py
@@ -21,32 +21,58 @@ test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_ind
 
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
+#
+# model = keras.Sequential()
+# model.add(keras.layers.Embedding(88000, 16))
+# model.add(keras.layers.GlobalAveragePooling1D())
+# model.add(keras.layers.Dense(16, activation="relu"))
+# model.add(keras.layers.Dense(1, activation="sigmoid"))
+#
+# model.summary()
+#
+# model.compile(optimizer = "adam", loss="binary_crossentropy", metrics=["accuracy"])
+#
+# x_val = train_data[:10000]
+# x_train = train_data[10000:]
+#
+# y_val = train_lables[:10000]
+# y_train = train_lables[10000:]
+#
+# fitmodel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+# results = model.evaluate(test_data, test_labels)
+# print(results)
+#
+# model.save("model.h5")
+#
+# test_review = test_data[99]
+# predict = model.predict([test_review])
+# print("Review: ")
+# print(decode_review(test_review))
+# print("Prediction: " + str(int(predict[0])))
+# print("Actual: " + str(test_labels[0]))
 
-model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
-model.add(keras.layers.GlobalAveragePooling1D())
-model.add(keras.layers.Dense(16, activation="relu"))
-model.add(keras.layers.Dense(1, activation="sigmoid"))
+def review_encode(s):
+    encoded = [1]
 
-model.summary()
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
 
-model.compile(optimizer = "adam", loss="binary_crossentropy", metrics=["accuracy"])
+    return encoded
 
-x_val = train_data[:10000]
-x_train = train_data[10000:]
 
-y_val = train_lables[:10000]
-y_train = train_lables[10000:]
+model = keras.models.load_model("model.h5")
 
-fitmodel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
-results = model.evaluate(test_data, test_labels)
 
-print(results)
-
-test_review = test_data[0]
-predict = model.predict([test_review])
-print("Review: ")
-print(decode_review(test_review))
-print("Prediction: " + str(predict[0]))
-print("Actual: " + str(test_labels[0]))
-print(results)
+with open("test.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        nline = line.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace("\"", "").replace(":", "").replace("?", "").replace("!", "").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post",
+                                                                maxlen=250)
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
